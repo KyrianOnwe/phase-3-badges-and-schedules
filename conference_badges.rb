61924e95237f44aa87e36ee9eb47ce1a577be0d9
Kyrian <dr.kyrian.onwe@gmail.com>
2022-11-09 23:27:07 -0600

    commit tree ee80da9a5aabf00af4a90cb276a2b94dbe98803a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array)
+    arr = array.map do |a|
+      "Hello, my name is #{a}."
+    end
+    return arr
+end
+
+def assign_rooms(array)
+    arrWindx = array.each_with_index{|value, index| "#{index}: #{value}"}
+    arr = arrWindx.map {|value, index| "Hello, #{value}! You'll be assigned to room #{index}"}
+    return arr
+end
